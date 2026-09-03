62bcca8997635866760681c3305d781abf645ccf
pjb3181-prog <pjb3181@gmail.com>
2026-09-03 16:16:28 +0900

    Bump web app cache for dashboard redesign

--- web/sw.js
+++ web/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "meri-schedule-v1";
+const CACHE = "meri-schedule-v2";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
