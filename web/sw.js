5cb4b1118e52c4f34985e35ccfcac75cce1bef6c
pjb3181-prog <pjb3181@gmail.com>
2026-09-03 16:29:28 +0900

    Cache work-calendar web assets

--- web/sw.js
+++ web/sw.js
@@ -1,5 +1,13 @@
-const CACHE = "meri-schedule-v2";
-const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest"];
+const CACHE = "meri-schedule-v3";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./work-calendar.css",
+  "./app.js",
+  "./work-calendar.js",
+  "./manifest.webmanifest"
+];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
