e6ca7e7989cd5a33fda0d00c725498330c3eae26
pjb3181-prog <pjb3181@gmail.com>
2026-09-03 15:50:22 +0900

    Add basic service worker

---
+++ web/sw.js
@@ -0,0 +1,17 @@
+const CACHE = "meri-schedule-v1";
+const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});
